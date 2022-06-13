9cafb5f4e155a3b80de77aa48e27f7eaeaa16067
Andres Biso <biso.andres@gmail.com>
2022-06-13 18:21:17 -0300

    feat: fix script_b.sh

--- tp_scripts/script_b.sh
+++ tp_scripts/script_b.sh
@@ -2,9 +2,8 @@
 #Escribir un script que muestre por consola la fecha del sistema cada 2 segundos, 10 veces. 
 #!/bash/sh
 
-while :; do
-  CURRENTDATE=`date`
-  echo "$CURRENTDATE"
-
+for i in {0..9}
+do
+  echo "$(date)"
   sleep 2
 done
